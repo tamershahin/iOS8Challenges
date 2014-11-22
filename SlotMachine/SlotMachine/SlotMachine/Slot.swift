6f7908f12afebe79266b8bc1837901c3a53fade7
Tamer <tamer.shahin@gmail.com>
2014-11-22 12:38:19 +0000

    till SlotMachine

---
+++ SlotMachine/SlotMachine/SlotMachine/Slot.swift
@@ -0,0 +1,16 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by Tamer Shahin on 22/11/2014.
+//  Copyright (c) 2014 Tasin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+    var value = 0
+    var image = UIImage(named: "Ace")
+    var isRed = true
+}
